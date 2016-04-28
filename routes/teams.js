a721d5f306eb0f032c5b176afd8cf54a95bc3970
MANUEL MORA <MANUEL MORA>
2016-04-28 15:08:09 -0500

    fixing error with request to api

--- routes/teams.js
+++ routes/teams.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var express = require('express');
 var ajax = require('ajax-request');
-var data = require('../data.json');
+//var data = require('../data.json');
 var router = express.Router();
 
 /* GET home page. */
@@ -14,16 +14,19 @@ router.get('/by-summoner/:summoner_id', function(req, res, next) {
   let region = 'lan';
   let url = 'https://lan.api.pvp.net/api/lol/' + region +
   '/v2.4/team/by-summoner/' + summID +
-  '&api_key=' + data.key;
+  '?api_key=80639ae1-4c81-4e81-a147-67ae8df88074';
 
   console.log('EXCECUTING REQUEST AT: ' + url);
+  
+  var request = require("request");
+  
 
-  ajax(url, function(err, res, body) {
+  request.get(url, function(err, res, body) {
     if(err) {
       console.log(err);
-      return;
+      
     }
-    res.send(body);
+    console.log(body);
   });
 });
 
